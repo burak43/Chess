be29f6e95e90dc0fc7898391fdab44840516cb81
berkevren <berkabbasoglu@hotmail.com>
2016-12-27 20:14:28 +0300

    Asked methods

--- Chess/src/model/Game.java
+++ Chess/src/model/Game.java
@@ -2,11 +2,21 @@ package model;
 
 public class Game {
 
+	GameManager gameManager;
 	/* input iki tane string berk burak al return olarak game objesi dön
 	 * Game g = new Game("burak", "berk");
 	 * bundan sonraki her method game objesinden çağırılacak, gameobjesi.ilgilimethod şeklinde */
 	public Game(String player1, String player2) {
-		
+
+		Player p1 = new Player(player1, Color.WHITE);
+		Player p2 = new Player(player2, Color.BLACK);
+
+		Player[] players = new Player[2];
+		players[0] = p1;
+		players[1] = p2;
+
+		gameManager = new GameManager(players);
+
 	}
 
 	/* method input yok, output bir tane array arrayin her elemanı ise üçlü array (2 boyutlu string array)
@@ -15,9 +25,30 @@ public class Game {
 	 * String[][] piecesPos = g.getPiecesPositions(); */
 	public String[][] getPiecesPositions() {
 		
-		String[][] go = new String[32][3];
-		
-		return go;
+		String[][] result = new String[32][3];
+		int count = 0;
+
+		for( int i = 0; i < 8; i++)
+		{
+			for(int j = 0; j < 8; j++)
+			{
+				char column = (char) ('A' + j);
+				if (gameManager.board.getSquareAtLoc(i, column).getPlacedPiece() != null) {
+
+					result[count][0] = gameManager.board.getSquareAtLoc(i, column).getPlacedPiece().getClass().getName();
+					result[count][1] = Character.toString(column);
+					result[count][2] = Integer.toString(i);
+					// if you want, you can also get the piece colour with the following
+					// but it returns a Color type so need to convert to string
+					//result[count][3] = gameManager.board.getSquareAtLoc(i, column).getPlacedPiece().getPieceColor();
+					count++;
+
+
+				}
+			}
+		}
+
+		return result;
 		
 	}
 	
@@ -25,10 +56,15 @@ public class Game {
 	 * (player 1 kazandı -> 1 / player 2 kazandı -> 2 / oyun devam ediyor -> 0)
 	 * int result = g.getStatus(); */
 	public int getStatus() {
-		
-		int stat = 0;
-		
-		return stat;
+
+		if(gameManager.result.equals("not finished"))
+			return 0;
+
+		if(gameManager.result.equals(gameManager.players[0].getName() + " is the Winner!"))
+			return 1;
+
+		if(gameManager.result.equals(gameManager.players[1].getName() + " is the Winner!"))
+			return 2;
 		
 	}
 	
@@ -38,20 +74,37 @@ public class Game {
 	 * input olarak aldığı pozisyonda taş yoksa boş array dönecek
 	 * String[][] possible = g.getPossible('A', 2); */
 	public String[][] getPossible(char cp, int ip) {
+
+		// CHAR SHOULD BE CAPITAL WHEN CALLED, like 'A', not 'a'
+		Square[] allPossible = gameManager.movementManager.validMoves(gameManager.board.getSquareAtLoc(ip, cp).getPlacedPiece());
+
+		String[][] squareToString = new String[64][2];
+
+		for (int i = 0; i < 64; i++) {
+			if (allPossible[i] == null) {
+				break;
+			}
+			squareToString[i][0] = Character.toString(allPossible[i].getColumn());
+			squareToString[i][1] = Integer.toString(allPossible[i].getRow());
+
+		}
 		
-		String[][] go = new String[64][2];
-		
-		return go;
+		return squareToString;
 		
 	}
 
 	/* method 4 input char int char int ('A', 2, 'A', 4), eğer a2 deki taşı a4 e götürebiliyorsa götürüp true dönecek; götüremiyorsa false dönecek
 	 * boolean gone = g.move('A', 2, 'A', 4); */
 	public boolean move(char cp1, int ip1, char cp2, int ip2) {
-		
-		boolean gone = false;
-		
-		return gone;
+
+		Piece pieceOfInterest = gameManager.board.getSquareAtLoc(ip1, cp1).getPlacedPiece();
+		Square destination = gameManager.board.getSquareAtLoc(ip2, cp2);
+
+
+		if (gameManager.movePiece(pieceOfInterest, destination) == null)
+			return false;
+
+		return true;
 		
 	}
 	
